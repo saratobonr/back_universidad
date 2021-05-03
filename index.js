b0f82c9c7ea02156baf55d1977c6323d9344e89c
saratobonres <60237855+saratobonres@users.noreply.github.com>
2021-05-03 17:35:40 -0500

    -m i

--- index.js
+++ index.js
@@ -4,9 +4,15 @@
 
  const express = require("express");
  const router = require('./app/routers/index');
+ const { patch } = require("./app/routers/index");
  const app = express();
+ const path = require("path");
  app.use(express.json());
  app.use("/", router);
+
+ app.use(express.static(path.join("./", "public")));
+ app.use("/documentos-excel", express.static(path.join("./", "dococumentos-excel")));
+
  const PORT =3001;
 
   app.listen(PORT, () => {
